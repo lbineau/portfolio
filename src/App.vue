ba432e9d7e7c1dbef2652067ce36c51a60028b79
Lory Bineau <lory.bineau@b-i.com>
2017-03-10 15:42:52 +0100

    add very basic portfolio

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <h1>Lbineau's portfolio</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Indie+Flower');
+
+html, body {
+  width: 100%;
+  height: 100%;
+}
+body {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #5586A2;
+}
+h1, h2, h3 {
+  font-family: 'Indie Flower', cursive;
+}
+</style>
+
+<style lang="scss" scoped>
+#app {
+  position: relative;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  flex-direction: column;
+
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+h1 {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  font-size: 3em;
+  transform: translateY(-100%);
+  padding-bottom: 1em;
+  text-align: center;
+  color: white;
+  @media (min-width: 480px) {
+    font-size: 5em;
+  }
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="hello">
+    <a href="https://github.com/lbineau/"><img src="../assets/icons/github.svg"></a>
+    <a href="https://www.linkedin.com/in/lbineau/"><img src="../assets/icons/linkedin.svg"></a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'hello'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.hello {
+  text-align: center;
+  /*display: flex;
+  align-content: center;
+  justify-content: center;*/
+}
+a {
+  display: inline-block;
+  padding: 1em;
+  &:hover {
+    img {
+      box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    }
+  }
+}
+img {
+  transition: box-shadow 0.3s ease-in-out;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  max-width: 100%;
+}
+</style>
