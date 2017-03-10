d61bb69096f569d2bcf03c3f3466441eb034e0f8
Lory Bineau <lory.bineau@b-i.com>
2017-03-10 21:50:09 +0100

    add redirects

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -92,6 +92,10 @@ var webpackConfig = merge(baseWebpackConfig, {
       {
         from: path.resolve(__dirname, '../manifest.json'),
         to: config.build.assetsRoot
+      },
+      {
+        from: path.resolve(__dirname, '../config/_redirects'),
+        to: config.build.assetsRoot
       }
     ]),
     new PrerenderSpaPlugin(
